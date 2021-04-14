e7e08ecf37b4d07f4db7aa936d217fabbb0073f3
iliweii <chall21@yeah.net>
2021-04-14 15:26:20 +0800

    更新主页

--- map/js/main.js
+++ map/js/main.js
@@ -1,32 +1,3 @@
-let portal = [
-    { x: -259, y: -94, msg: "The World305 蜘蛛刷怪笼" },
-    { x: -87, y: 1, msg: "Wangs121 水中小屋" },
-    { x: -81, y: -9, msg: "UnkemptSphere64'home" },
-    { x: -81, y: 1, msg: "某冰山" },
-    { x: -39, y: 42, msg: "啊妙的家" },
-    { x: -38, y: 13, msg: "尖啸之门" },
-    { x: -18, y: 42, msg: "Luotian的交易市场" },
-    { x: 94, y: 32, msg: "挂机基地" },
-]
-let node = [
-    { x: -259, y: 47 },
-    { x: -81, y: 47 },
-    { x: -38, y: 47 },
-    { x: 18, y: 47 },
-    { x: 94, y: 47 },
-    { x: -81, y: 1 },
-    { x: -81, y: -28 },
-    { x: -38, y: 42 },
-]
-
 $(function () {
     $(".subway-map").subwayMap({ debug: true });
 })
-
-function x(n) {
-
-}
-
-function z(n) {
-
-}
